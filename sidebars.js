b18872c56a3b994c6c2da5f77808c7481434d5e5
mohammad <m.sabah.farhadi@gmail.com>
2021-04-03 10:19:29 +0430

    change sidbars.js

--- sidebars.js
+++ sidebars.js
@@ -1,8 +1,10 @@
 module.exports = {
   someSidebar: {
-    "تجربه های ما": ["ourExperience/HowWriteMDX"],
+    "تجربه های ما": ["ourExperience/HowWriteMDX", {
+      "Docker": ["ourExperience/docker/docker-mf"],
+      "PWA": ["ourExperience/PWA/pwa", "ourExperience/PWA/pwa_react"]
+    }],
     "داکیومنت های شرکت": ["hami/test"],
-    "PWA": ['PWA/pwa', 'PWA/pwa_react'],
     Docusaurus: ['docusaurus/doc1', 'docusaurus/doc2', 'docusaurus/doc3', 'docusaurus/mdx'],
   },
 };
