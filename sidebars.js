9f878a53911f518f4b3eeefb0e5b8e5199da7f48
mohammad farhadi <m.sabah.farhadi@gmail.com>
2021-04-05 23:55:34 +0430

    add tailwind

--- sidebars.js
+++ sidebars.js
@@ -1,7 +1,26 @@
 module.exports = {
-  someSidebar: {
-    "تجربه های ما": ["ourExperience/HowWriteMDX"],
-    "داکیومنت های شرکت": ["hami/test"],
-    Docusaurus: ['docusaurus/doc1', 'docusaurus/doc2', 'docusaurus/doc3', 'docusaurus/mdx'],
-  },
+    someSidebar: {
+        "تجربه های ما": [
+            "ourExperience/HowWriteMDX",
+            {
+                type: 'category',
+                label: 'Tailwind in Next.js',
+                items: [
+                    'ourExperience/tailwind/tailwind-next',
+                    'ourExperience/tailwind/custom-tailwind-style',
+                    'ourExperience/tailwind/responsive',
+                    'ourExperience/tailwind/hover-focus-etc',
+                ],
+            },
+        ],
+        "داکیومنت های شرکت": [
+            "hami/test",
+        ],
+        Docusaurus: [
+            'docusaurus/doc1',
+            'docusaurus/doc2',
+            'docusaurus/doc3',
+            'docusaurus/mdx',
+        ],
+    },
 };
