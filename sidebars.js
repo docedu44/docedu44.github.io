b806cf60061bea904f646be1a47babbad80b6565
sinasafari <m.sinasafai@gmail.com>
2021-04-13 13:16:50 +0430

    redux tool kit added, description needed

--- sidebars.js
+++ sidebars.js
@@ -2,7 +2,8 @@ module.exports = {
   someSidebar: {
     "تجربه های ما": ["ourExperience/HowWriteMDX", {
       "Docker": ["ourExperience/docker/docker-mf"],
-      "PWA": ["ourExperience/PWA/pwa", "ourExperience/PWA/pwa_react"]
+      "PWA": ["ourExperience/PWA/pwa", "ourExperience/PWA/pwa_react"],
+      "redux tool kit": ["ourExperience/redux-tool-kit/reduxtoolkit"]
     }],
     "داکیومنت های شرکت": ["hami/test"],
     Docusaurus: ['docusaurus/doc1', 'docusaurus/doc2', 'docusaurus/doc3', 'docusaurus/mdx'],
